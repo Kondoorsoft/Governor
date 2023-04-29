33957889d07d59f8001d1c4e93dd68de22d12eb7
Jeremy Wilson <jeremydwilson4500@gmail.com>
2023-04-29 12:41:55 -0400

    Latest code

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -2,16 +2,16 @@ extends Node2D
 
 @onready var data := preload("res://scenes/Sprites/Data.tscn")
 @onready var action_window: Area2D = $FullWindow/Center/ActionWindow
+@onready var background: Sprite2D = $FullWindow/Background
 
 var timer: Timer
-var current_spawn_index := 0
-var enemy_spawn: Vector2
+var spawn_point: Vector2
 
 func _ready():
 
 	action_window.connect('body_exited', Callable(self, 'data_missed'))
 
-	enemy_spawn = Vector2(20, get_viewport_rect().size.y / 2)
+	spawn_point = Vector2(20, get_viewport_rect().size.y / 2)
 
 	timer = Timer.new()
 	timer.set_wait_time(1)
@@ -21,39 +21,44 @@ func _ready():
 	timer.start()
 
 func _process(_delta):
-	var collisions := action_window.get_overlapping_bodies()
+	var collisions := action_window.get_overlapping_bodies().filter(unprocessed)
 	var next_data: CharacterBody2D = null
-	if len(Globals.spawned) > 0 && len(collisions) > 0:
-		for obj in collisions:
-			if obj.get('spawn_index') != null && obj.spawn_index == Globals.spawned[0]:
-				next_data = obj
-				break
+	if len(collisions) > 0:
+		next_data = collisions[0]
 
-	if Input.is_action_just_pressed('primary_up') && next_data != null:
-		Globals.spawned.pop_front()
-		Globals.set_velocity(Globals.VELOCITIES.north)
-		next_data = null
+	if next_data != null:
+		if Input.is_action_just_pressed('northwest'):
+			Globals.set_velocity(next_data, Globals.VELOCITIES.northwest)
+			background.frame = 4
+		elif Input.is_action_just_pressed('north'):
+			Globals.set_velocity(next_data, Globals.VELOCITIES.north)
+			background.frame = 3
+		elif Input.is_action_just_pressed('northeast'):
+			Globals.set_velocity(next_data, Globals.VELOCITIES.northeast)
+			background.frame = 2
+		elif Input.is_action_just_pressed('east'):
+			Globals.set_velocity(next_data, Globals.VELOCITIES.east)
+			background.frame = 1
+		elif Input.is_action_just_pressed('southeast'):
+			Globals.set_velocity(next_data, Globals.VELOCITIES.southeast)
+			background.frame = 0
+		elif Input.is_action_just_pressed('south'):
+			Globals.set_velocity(next_data, Globals.VELOCITIES.south)
+			background.frame = 6
+		elif Input.is_action_just_pressed('southwest'):
+			Globals.set_velocity(next_data, Globals.VELOCITIES.southwest)
+			background.frame = 5
 
-
-	elif Input.is_action_just_pressed('primary_right') && next_data != null:
-		Globals.spawned.pop_front()
-		Globals.set_velocity(Globals.VELOCITIES.east)
-		next_data = null
-
-	elif Input.is_action_just_pressed('primary_down') && next_data != null:
-		Globals.spawned.pop_front()
-		Globals.set_velocity(Globals.VELOCITIES.south)
-		next_data = null
+func unprocessed(body: Node2D):
+	return body.get('processed') == false
 	
 func spawn_data():
-	var enemy = data.instantiate()
-	enemy.spawn_index = current_spawn_index
-	Globals.spawned.append(current_spawn_index)
-	current_spawn_index += 1
-	enemy.position = enemy_spawn
-	add_child(enemy)
-	timer.set_wait_time(5)
+	var sprite = data.instantiate()
+	sprite.position = spawn_point
+	add_child(sprite)
+	timer.set_wait_time(0.5)
 	timer.start()
 
 func data_missed(body: Node2D):
-	body.current_velocity = Globals.current_velocity
+	body.set('current_velocity', Globals.current_velocity)
+	body.set('processed', true)
--- scripts/autoloads/Globals.gd
+++ scripts/autoloads/Globals.gd
@@ -1,15 +1,21 @@
 extends Node
 
-const DEFAULT_SPEED := 100
+const DEFAULT_SPEED := 200
 const VELOCITIES := {
+    "northwest": Vector2(DEFAULT_SPEED * -1, DEFAULT_SPEED * -1),
     "north": Vector2(0, DEFAULT_SPEED * -1),
+    "northeast": Vector2(DEFAULT_SPEED, DEFAULT_SPEED * -1),
+    "east": Vector2(DEFAULT_SPEED, 0),
+    "southeast": Vector2(DEFAULT_SPEED, DEFAULT_SPEED),
     "south": Vector2(0, DEFAULT_SPEED),
-    "east": Vector2(DEFAULT_SPEED, 0)
+    "southwest": Vector2(DEFAULT_SPEED * -1, DEFAULT_SPEED),
 }
 const INITIAL_VELOCITY := VELOCITIES.east
 
 var spawned: Array[int] = []
 var current_velocity: Vector2 = VELOCITIES.south
 
-func set_velocity(new_velocity: Vector2):
+func set_velocity(body: Node2D, new_velocity: Vector2):
+    body.set('current_velocity', new_velocity)
+    body.set('processed', true)
     current_velocity = new_velocity
