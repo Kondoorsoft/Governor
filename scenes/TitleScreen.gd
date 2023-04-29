e87195b22f0b5dce5d4d3b04a9207608754b2fe3
Trent Council <dev@trentcouncil.com>
2023-04-29 13:48:41 -0400

    Added functioning menu

--- scenes/TitleScreen.gd
+++ scenes/TitleScreen.gd
@@ -8,3 +8,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _physics_process(delta: float) -> void:	
+	if Input.is_action_pressed('ui_accept'):
+		get_tree().change_scene_to_file('res://scenes/Opening.tscn')
