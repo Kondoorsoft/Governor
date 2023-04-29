554511bc24df7d0377ba10c0f450fcceedf914dd
Jeremy Wilson <jeremydwilson4500@gmail.com>
2023-04-29 18:43:48 -0400

    Changed variable

--- scenes/Sprites/Data.gd
+++ scenes/Sprites/Data.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @export var spawn_index: int
 @export var current_velocity: Vector2 = Globals.INITIAL_VELOCITY
-@export var processed := false
+@export var waiting_instruction := false
 
 func _process(delta: float) -> void:
 	move_and_collide(Vector2(current_velocity.x * delta, current_velocity.y * delta))
